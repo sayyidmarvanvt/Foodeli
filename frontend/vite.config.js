c27ae2489feb27eb35ea4cc9dbc51abcbdaa65eb
Sayyid Marvan <150991120+sayyidmarvanvt@users.noreply.github.com>
2024-08-10 13:33:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://foodeli-backend-55b2.onrender.com/",
+        target: "https://foodeli-backend-55b2.onrender.com",
         secure: false,
       },
     },

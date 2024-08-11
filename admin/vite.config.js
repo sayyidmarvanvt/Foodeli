145554bccc318a815b559ceeccf633ed0cae728a
Sayyid Marvan <150991120+sayyidmarvanvt@users.noreply.github.com>
2024-08-11 12:45:40 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     // "/api": {
-  //     //   target: "https://foodeli-backend-55b2.onrender.com",
-  //     //   secure: false,
-  //     // },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://foodeli-backend-55b2.onrender.com",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 });

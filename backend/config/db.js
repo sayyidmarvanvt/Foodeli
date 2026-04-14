0ff4ccbbac4c34e63571436d7694e536eb698b13
marvan-srv <sayyid@srvithub.com>
2026-04-14 14:24:28 +0530

    update mongoUrl

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
 
 export const connectDB=async ()=>{
-    await mongoose.connect('mongodb+srv://sayyidmarvanvt:AHVQiDfyixeukJzq@cluster0.pwpw7sr.mongodb.net/foodeli').then(()=>console.log("DB Connected"))
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"))
 }
